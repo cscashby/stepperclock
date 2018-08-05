65498b2e6521938e72324d3dc4e17f64a9d3180e
Christian Ashby <git@cashby.me.uk>
2018-08-05 18:04:14 +0000

    First working implementation, needs calibration

--- clock.py
+++ clock.py
@@ -7,27 +7,18 @@ import sys, traceback
 from pythonosc import dispatcher
 from pythonosc import osc_server
 
-STEP_DELAY = 0.0030     # delay between steps 
-STEPS_24HRS = 3550      # total number of steps in 24 hours on the clock face
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
+STEP_DELAY = 0.0030     # STEP_DELAY between steps 
+STEPS_24HRS = 6800      # total number of steps in 24 hours on the clock face
 
 OSC_LISTEN = "0.0.0.0"  # default listen on address (0.0.0.0 = all)
 OSC_PORT = 5005         # default listen on address (0.0.0.0 = all)
 
 # NOTE no need for enable PINs; they can be constantly pulled high
 # IN1-4 to control step sequence
-COIL_A_1_PIN = 17
-COIL_A_2_PIN = 18
-COIL_B_1_PIN = 22
-COIL_B_2_PIN = 23
-
-# Set PIN states
-GPIO.setup(COIL_A_1_PIN, GPIO.OUT)
-GPIO.setup(COIL_A_2_PIN, GPIO.OUT)
-GPIO.setup(COIL_B_1_PIN, GPIO.OUT)
-GPIO.setup(COIL_B_2_PIN, GPIO.OUT)
+COIL_B_1_PIN = 17
+COIL_B_2_PIN = 18
+COIL_A_1_PIN = 22
+COIL_A_2_PIN = 23
 
 # Function for step sequence
 def setStep(w1, w2, w3, w4):
@@ -39,30 +30,32 @@ def setStep(w1, w2, w3, w4):
 def runBackward(steps):
     for i in range(0, steps):
         setStep(1,0,1,0)
-        time.sleep(delay)
+        time.sleep(STEP_DELAY)
         setStep(0,1,1,0)
-        time.sleep(delay)
+        time.sleep(STEP_DELAY)
         setStep(0,1,0,1)
-        time.sleep(delay)
+        time.sleep(STEP_DELAY)
         setStep(1,0,0,1)
-        time.sleep(delay)
+        time.sleep(STEP_DELAY)
 
 def runForward(steps):
     for i in range(0, steps):
         setStep(1,0,0,1)
-        time.sleep(delay)
+        time.sleep(STEP_DELAY)
         setStep(0,1,0,1)
-        time.sleep(delay)
+        time.sleep(STEP_DELAY)
         setStep(0,1,1,0)
-        time.sleep(delay)
+        time.sleep(STEP_DELAY)
         setStep(1,0,1,0)
-        time.sleep(delay)
+        time.sleep(STEP_DELAY)
 
-def forward_handler(unused_addr, args, time):
-    print("forward {}".format(args))
+def forward_handler(unused_addr, args, mins):
+    print("forward {} for {} mins".format(args, mins))
+    runForward(mins * int(STEPS_24HRS/24/60))
 
-def backward_handler(unused_addr, args, time):
-    print("backward {}".format(args))
+def backward_handler(unused_addr, args, mins):
+    print("backward {} for {} mins".format(args, mins))
+    runBackward(mins * int(STEPS_24HRS/24/60))
 
 def preset_handler(unused_addr, args):
     print("preset {}".format(args))
@@ -72,6 +65,14 @@ def stop_handler(unused_addr, args):
 
 def main():
     try:
+        # Set PIN states
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
+        GPIO.setup(COIL_A_1_PIN, GPIO.OUT)
+        GPIO.setup(COIL_A_2_PIN, GPIO.OUT)
+        GPIO.setup(COIL_B_1_PIN, GPIO.OUT)
+        GPIO.setup(COIL_B_2_PIN, GPIO.OUT)
+
         d = dispatcher.Dispatcher()
         d.map("/forward", forward_handler, "Clock forwards")
         d.map("/backward", backward_handler, "Clock backwards")
