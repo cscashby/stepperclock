4fc8439ca9d3db83882939d737fa84a7be3d9691
Christian Ashby <git@cashby.me.uk>
2018-08-05 16:06:04 +0000

    Added stop code to test to ensure motor not energised, fixed caps in clock.py

--- clock.py
+++ clock.py
@@ -9,25 +9,25 @@ STEPS_24HRS = 3550      # total number of steps in 24 hours on the clock face
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
-# NOTE no need for enable pins; they can be constantly pulled high
+# NOTE no need for enable PINs; they can be constantly pulled high
 # IN1-4 to control step sequence
 COIL_A_1_PIN = 17
 COIL_A_2_PIN = 18
 COIL_B_1_PIN = 22
 COIL_B_2_PIN = 23
 
-# Set pin states
-GPIO.setup(coil_A_1_pin, GPIO.OUT)
-GPIO.setup(coil_A_2_pin, GPIO.OUT)
-GPIO.setup(coil_B_1_pin, GPIO.OUT)
-GPIO.setup(coil_B_2_pin, GPIO.OUT)
+# Set PIN states
+GPIO.setup(COIL_A_1_PIN, GPIO.OUT)
+GPIO.setup(COIL_A_2_PIN, GPIO.OUT)
+GPIO.setup(COIL_B_1_PIN, GPIO.OUT)
+GPIO.setup(COIL_B_2_PIN, GPIO.OUT)
 
 # Function for step sequence
 def setStep(w1, w2, w3, w4):
-  GPIO.output(coil_A_1_pin, w1)
-  GPIO.output(coil_A_2_pin, w2)
-  GPIO.output(coil_B_1_pin, w3)
-  GPIO.output(coil_B_2_pin, w4)
+  GPIO.output(COIL_A_1_PIN, w1)
+  GPIO.output(COIL_A_2_PIN, w2)
+  GPIO.output(COIL_B_1_PIN, w3)
+  GPIO.output(COIL_B_2_PIN, w4)
 
 def runBackward(steps):
     for i in range(0, steps):
